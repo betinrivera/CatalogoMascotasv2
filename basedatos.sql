20d27ab60f0f61c9e968b53a3d155fe0385b026c
Alumno <ulsa@iMacD113.local>
2019-02-06 18:32:05 -0700

    BaseDeDatosActualizada

--- basedatos.sql
+++ basedatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 17, 2019 at 07:39 PM
+-- Generation Time: Feb 07, 2019 at 01:31 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `especies`
+-- Database: `mascotas`
 --
 
 -- --------------------------------------------------------
@@ -48,8 +48,19 @@ CREATE TABLE `mascotas` (
   `nombre` varchar(255) NOT NULL,
   `precio` int(11) NOT NULL,
   `nacimiento` date NOT NULL,
-  `foto` varchar(255) NOT NULL
+  `foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `mascotas`
+--
+
+INSERT INTO `mascotas` (`id`, `id_especie`, `nombre`, `precio`, `nacimiento`, `foto`) VALUES
+(1, 1, 'Miaumiau', 20000, '2019-02-28', NULL),
+(2, 4, 'Pancho', 15000, '2019-02-08', NULL),
+(4, 5, 'Nemo', 5000, '2018-07-10', NULL),
+(5, 2, 'Iram', 12500, '2018-05-13', NULL),
+(6, 4, 'Babo', 120, '2018-08-17', NULL);
 
 --
 -- Indexes for dumped tables
@@ -82,7 +93,7 @@ ALTER TABLE `especies`
 -- AUTO_INCREMENT for table `mascotas`
 --
 ALTER TABLE `mascotas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -93,4 +104,3 @@ ALTER TABLE `mascotas`
 --
 ALTER TABLE `mascotas`
   ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`id_especie`) REFERENCES `especies` (`id`);
-
